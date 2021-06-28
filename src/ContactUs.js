be0af6bf45ef1a7bdd0a105efae591a41dec259e
Ashish Poudel <ashishpoudel995@gmail.com>
2021-06-28 22:21:40 +0545

    Codes updated

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,33 +1,12 @@
 const ContactUs = () => {
   return (
-    <div>
-      <div className="contact-us">
-        <div className="customerservice">
-          <h3>Customer Service</h3>
-          <p>
-            <i class="fas fa-envelope"></i>&nbsp;customer@hempin.com <br />
-            <i class="fas fa-phone-alt"></i>&nbsp;+919003496599
-          </p>
+    <div className="contact-us">
+      <div className="card">
+        <h3>Customer Service</h3>
+        <div>
+          <i class="fas fa-envelope"></i>&nbsp;customer@hempin.com <br />
+          <i class="fas fa-phone-alt"></i>&nbsp;+91 9999999999
         </div>
-        <div className="sales">
-          <h3>Sales</h3>
-          <p>
-            <i class="fas fa-envelope"></i>&nbsp;sales@hempin.com
-            <br />
-            <i class="fas fa-phone-alt"></i>&nbsp;+919003496599
-          </p>
-        </div>
-        <div className="returnsandrefund">
-          <h3>Returns and Refund</h3>
-          <p>
-            <i class="fas fa-envelope"></i>&nbsp;returns@hempin.com
-            <br />
-            <i class="fas fa-phone-alt"></i>&nbsp;+919003496599
-          </p>
-        </div>
-      </div>
-      <div className="hempin-quote">
-        For Public | For Profit | For Society and Environment
       </div>
     </div>
   );
